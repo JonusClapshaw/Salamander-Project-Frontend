063ed071f48fe12be0218db348e67e073015cf2a
anthonykravchishin <tonykravc53@gmail.com>
2026-05-14 10:30:29 -0700

    Created Mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx'
 import Videos from './pages/Video.jsx';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/videos" element={<Videos />} />
-    </Routes>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/videos">Videos</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+      </Routes>
+    </div>
   );
 }
