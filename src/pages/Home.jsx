455d7bd611a044d0715d6d27b7c0a9fa466132dc
anthonykravchishin <tonykravc53@gmail.com>
2026-05-14 11:05:25 -0700

    Some styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-blue-600">Salamander Tracker</h1>
-      <p>Pick a video from the Videos page to start analyzing.</p>
+      <div className="
+        mx-20
+        text-center
+        border
+      ">
+        <h1 className="
+          text-4xl 
+          font-bold 
+          text-blue-600
+        ">Salamander Tracker</h1>
+        <p>Pick a video from the Videos page to start analyzing.</p>
+      </div>
+      <img src="https://cdn.britannica.com/64/239564-050-522E431D/Western-tiger-salamander-Ambystoma-mavortium.jpg"
+          className="
+            w-100
+            text-center
+          "></img>
     </div>
   );
 }
