3da95b4bf0524ce14ce975d2a0d4f5cc3ce054bf
Jonus <joclapshaw@gmail.com>
2026-05-14 10:39:12 -0700

    Changes to videos page

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,17 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getVideos } from '../mockApi.js';
 
 export default function Videos() {
+  const [videos, setVideos] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     getVideos().then((data) => {
-      console.log("getVideos returned:", data);
+        setLoading(false);
+        setVideos(data)
+    }).catch((err) => {
+        setError(err.message);
+        setLoading(false);
     });
   }, []);
+
+  if(loading) {
+    return<p>Loading videos...</p>
+  }
+
+  if(error) {
+    return <p>Could not load videos: {error}</p>
+  }
 
   return (
     <div>
       <h1>Available Videos</h1>
-      <p>Video list will go here.</p>
+      <ul>
+        {videos.map((filename) => (
+          <li key={filename}>{filename}</li>
+        ))}
+      </ul>
     </div>
   );
 }
