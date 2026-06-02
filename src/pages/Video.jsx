158c3ed57f0ec379ebe771e42afbab615b81d10a
Jonus <joclapshaw@gmail.com>
2026-06-02 10:20:48 -0700

    Swapping mock data

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getThumbnail, getVideos } from '../mockApi.js';
+import { getThumbnail, getVideos } from '../api.js';
 
 export default function Videos() {
   const [videos, setVideos] = useState([]);
